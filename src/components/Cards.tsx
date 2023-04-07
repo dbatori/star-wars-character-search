f20b934283a2ccf1097ef53cdc8c1a2caf73cc38
Daniel Batori <daniel@batori.dev>
2023-04-07 15:46:41 +0200

    Align cards inside a grid

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,8 +1,8 @@
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
 import { Character } from "../api";
 
 type Props = {
@@ -14,20 +14,29 @@ type Props = {
 
 export const Cards = (props: Props) => (
   <>
-    <Box display="flex" flexWrap="wrap" gap={2} py={1}>
+    <Grid container spacing={2}>
       {props.results.map(({ name }, idx) => (
-        <Card key={name} sx={{ padding: 2, width: "100%", maxWidth: 250 }}>
-          <CardMedia
-            sx={{ height: "75vw", maxHeight: 250, backgroundSize: "contain" }}
-            image={idx % 2 === 0 ? "/mock-image-1.png" : "/mock-image.png"}
-            title={name}
-          />
-          <Typography variant="body2" textAlign="center">
-            {name}
-          </Typography>
-        </Card>
+        <Grid key={name} item xs={12} sm={6} md={4} lg={3}>
+          <Card
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              padding: 3,
+            }}
+          >
+            <CardMedia
+              sx={{ height: 250, backgroundSize: "contain" }}
+              image={idx % 2 === 0 ? "/mock-image-1.png" : "/mock-image.png"}
+              title={name}
+            />
+            <Typography variant="body2" textAlign="center">
+              {name}
+            </Typography>
+          </Card>
+        </Grid>
       ))}
-    </Box>
+    </Grid>
     {props.results.length < props.total && (
       <Button
         variant="outlined"
